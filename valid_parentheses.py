4bcd4e1f68a5a4101683092ed74791b62e95e42f
Xinhang Shi <shixinhanghust@gmail.com>
2017-05-01 16:09:02 +0800

    create isValid1 for extension
    Add isValid1 and test_isValid1 when only '(' and ')' with constant extra
    space.

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -12,5 +12,25 @@ class Solution(object):
             else:
                 if len(stack) == 0 or char != dict[stack.pop()]:
                     return False
-        else:
-            return len(stack) == 0
+        
+		return len(stack) == 0
+			
+			
+    def isValid1(self, s):
+        """
+        extension: only ( and ) with constant extra space.
+        """
+		counter = 0
+        for char in s:
+            if char == '(':
+				counter += 1
+			elif char == ')':
+				if counter == 0:
+					return False
+				counter -= 1
+				
+		return counter == 0
+			
+	def test_isValid1():
+		for s in ['', '(', ')', '())', '(()', '(()))', '((())', '())(()']:
+			print(s, isValid1(s))
